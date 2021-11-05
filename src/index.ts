3ba2d85d4d8cc16624fd506bc237b7d9341d2eb6
Authur <504151911@qq.com>
2021-11-05 14:38:16 +0800

    init

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+// import * as process from 'process';
+// import * as lib from './lib/index';
+// const argv = process.argv;
+// const command = argv[2];
+// if (command) {
+//   if (lib[command]) {
+//     lib[command]();
+//   } else {
+//     console.log('命令不存在');
+//   }
+// } else {
+//   console.log('请输入命令');
+// }
